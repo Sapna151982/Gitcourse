4d412959f7328db61b1b542509dd9f5dbea45d3e
Sapna151982 <Sapna.Aggarwal@cognizant.com>
2025-07-31 23:30:32 -0700

    Add hello_git.py with greeting function

---
+++ hello_git.py
@@ -0,0 +1,9 @@
+def greet():
+    """
+    A simple function to print a greeting message
+    """
+    print("Hello from Git!")
+    print("This is my first Git project")
+
+if __name__ == "__main__":
+    greet()
